521fd9a7bfbf3d9943a0188dff9655f65c03304b
bhoon-coding <bhooncoding@gmail.com>
2024-08-13 16:32:19 +0900

    Initial Commit

---
+++ TuistTutorial/TuistTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuistTutorialApp.swift
+//  TuistTutorial
+//
+//  Created by BH on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct TuistTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
